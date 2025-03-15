7dbb70744edd3c694a9e373f13f75120df7cc369
Gopal Ji <139383848+Gopal2210G@users.noreply.github.com>
2025-03-15 19:25:45 +0530

    first commit

---
+++ HighFive/config.py
@@ -0,0 +1,11 @@
+SECRET_KEY = 'rkgal'
+
+DB_CONFIG = {
+    'dbname': '22CS30065',
+    'user': '22CS30065',
+    'password': '22CS30065',
+    'host': 'localhost',
+    'port': '5432'
+}
+
+PATH_NAME = 'highfive'
